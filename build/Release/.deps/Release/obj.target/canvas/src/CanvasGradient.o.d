6417e032e3657730b45b9d58f4e795f5f6b9a405
Maurits Lamers <maurits@weidestraat.nl>
2012-09-29 12:57:09 +0200

    first commit of working binary packages

---
+++ build/Release/.deps/Release/obj.target/canvas/src/CanvasGradient.o.d
@@ -0,0 +1,40 @@
+cmd_Release/obj.target/canvas/src/CanvasGradient.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D_DARWIN_USE_64_BIT_INODE=1' '-DHAVE_JPEG' -I/Users/maurits/.node-gyp/0.8.10/src -I/Users/maurits/.node-gyp/0.8.10/deps/uv/include -I/Users/maurits/.node-gyp/0.8.10/deps/v8/include -I../usr/local/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/canvas/src/CanvasGradient.o.d.raw  -c -o Release/obj.target/canvas/src/CanvasGradient.o ../src/CanvasGradient.cc
+Release/obj.target/canvas/src/CanvasGradient.o: ../src/CanvasGradient.cc \
+  ../src/color.h ../src/Canvas.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8stdint.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares_version.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/uv-unix.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ev.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/eio.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node.h \
+  /Users/maurits/.node-gyp/0.8.10/src/ev-emul.h \
+  /Users/maurits/.node-gyp/0.8.10/src/eio-emul.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node_version.h \
+  ../src/CanvasGradient.h
+../src/CanvasGradient.cc:
+../src/color.h:
+../src/Canvas.h:
+/Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8.h:
+/Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8stdint.h:
+/Users/maurits/.node-gyp/0.8.10/src/node.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares_version.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/uv-unix.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ngx-queue.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ev.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/eio.h:
+/Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h:
+/Users/maurits/.node-gyp/0.8.10/src/node.h:
+/Users/maurits/.node-gyp/0.8.10/src/ev-emul.h:
+/Users/maurits/.node-gyp/0.8.10/src/eio-emul.h:
+/Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h:
+/Users/maurits/.node-gyp/0.8.10/src/node_version.h:
+../src/CanvasGradient.h:
---
+++ build/Release/.deps/Release/obj.target/canvas/src/ImageData.o.d
@@ -0,0 +1,39 @@
+cmd_Release/obj.target/canvas/src/ImageData.o := g++ '-D_LARGEFILE_SOURCE' '-D_FILE_OFFSET_BITS=64' '-D_DARWIN_USE_64_BIT_INODE=1' '-DHAVE_JPEG' -I/Users/maurits/.node-gyp/0.8.10/src -I/Users/maurits/.node-gyp/0.8.10/deps/uv/include -I/Users/maurits/.node-gyp/0.8.10/deps/v8/include -I../usr/local/include  -Os -gdwarf-2 -mmacosx-version-min=10.5 -arch x86_64 -Wall -Wendif-labels -W -Wno-unused-parameter -fno-rtti -fno-exceptions -fno-threadsafe-statics -fno-strict-aliasing -MMD -MF ./Release/.deps/Release/obj.target/canvas/src/ImageData.o.d.raw  -c -o Release/obj.target/canvas/src/ImageData.o ../src/ImageData.cc
+Release/obj.target/canvas/src/ImageData.o: ../src/ImageData.cc \
+  ../src/ImageData.h ../src/Canvas.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8stdint.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares_version.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/uv-unix.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ngx-queue.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ev.h \
+  /Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/eio.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node.h \
+  /Users/maurits/.node-gyp/0.8.10/src/ev-emul.h \
+  /Users/maurits/.node-gyp/0.8.10/src/eio-emul.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h \
+  /Users/maurits/.node-gyp/0.8.10/src/node_version.h ../src/PixelArray.h
+../src/ImageData.cc:
+../src/ImageData.h:
+../src/Canvas.h:
+/Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8.h:
+/Users/maurits/.node-gyp/0.8.10/deps/v8/include/v8stdint.h:
+/Users/maurits/.node-gyp/0.8.10/src/node.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/ares_version.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/uv-unix.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ngx-queue.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/ev.h:
+/Users/maurits/.node-gyp/0.8.10/deps/uv/include/uv-private/eio.h:
+/Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h:
+/Users/maurits/.node-gyp/0.8.10/src/node.h:
+/Users/maurits/.node-gyp/0.8.10/src/ev-emul.h:
+/Users/maurits/.node-gyp/0.8.10/src/eio-emul.h:
+/Users/maurits/.node-gyp/0.8.10/src/node_object_wrap.h:
+/Users/maurits/.node-gyp/0.8.10/src/node_version.h:
+../src/PixelArray.h:
